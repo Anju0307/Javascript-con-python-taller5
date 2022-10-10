621ab8ee92d99d7852a69f5600efbe856fc65215
Anju0307 <jpmantilla01@misena.edu.co>
2022-10-10 14:42:34 -0500

    Ejercicio 2

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 addEventListener("DOMContentLoaded", (e) => {
+    let suma = 0,des;
     let form = document.querySelector("#form");
     document.querySelector("#numero1").readOnly = false;
-    document.querySelector("#numero2").readOnly = false;
     document.querySelector("#boton").disabled = false;
+    let contenedor = document.querySelector("#contenedor");
+    let resultado = document.querySelector("#resultado");
+    let deshabilitar = () => {
+        contenedor.style.display = "inline-block";
+        document.querySelector("#numero1").readOnly = true;
+        document.querySelector("#boton").disabled = true;
+    }
+    let descuento = () => {
+        (suma>10000)
+            ? (des =suma-(suma*0.10))
+            (resultado.insertAdjacentHTML("beforebegin", `<h2>Se le aplica un descuento del 10% y el total a pagar es de: ${des}<h2/>`))
+            : (resultado.insertAdjacentHTML("beforebegin", `<h2>${suma}<h2/>`));
+
+    }
     form.addEventListener("submit", (e) => {
         e.preventDefault();
         let num1 = parseInt(document.querySelector("#numero1").value);
-        let num2 = parseInt(document.querySelector("#numero2").value);
-        let suma = num1 + num2;
-        let resultado = document.querySelector("#resultado");
-        resultado.insertAdjacentHTML("beforebegin", `<h2>${suma}<h2/>`);
-        let contenedor = document.querySelector("#contenedor").style.display = 'inline-block';
-        document.querySelector("#numero1").readOnly = true;
-        document.querySelector("#numero2").readOnly = true;
-        document.querySelector("#boton").disabled = true;
-        document.querySelector("#boton").style.cursor ="not-allowed";
         form.reset();
-        contenedor.addEventListener("submit", (e) => {
-            contenedor == true ? contenedor.style.display = 'none' : contenedor.style.display = 'inline-block';
-            document.querySelector("#numero1").readOnly = false;
-            document.querySelector("#numero2").readOnly = false;
-            document.querySelector("#boton").disabled = false;
-        })
+        (num1 > 0)
+            ? (suma += num1)
+            : (num1 < 0) ?
+                (alert("No puedes ingresar un numero menor"))
+                : (deshabilitar())
+                    (descuento());
     })
 })
