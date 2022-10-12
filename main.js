0159b6d4011353c85974cc0085bd138cb5d7dcca
Anju0307 <jpmantilla01@misena.edu.co>
2022-10-12 12:03:58 -0500

    Ejecicio 4

--- main.js
+++ main.js
@@ -3,45 +3,20 @@ addEventListener("DOMContentLoaded", (e) => {
         contenedor.style.display = "inline-block";
         document.querySelector("#boton").disabled = true;
     }
-    function pagar(checkboxData,select){
-        let totalPagar = 0;
-        checkboxData.forEach(ingre=>{
-            ingre=="Pepinillos" ? totalPagar+=150: null;
-            ingre=="Champiñones" ? totalPagar+=150: null;
-            ingre=="Cebolla" ? totalPagar+=150: null;
-        });
-        switch(select){
-            case "peque":
-                totalPagar += 500;
-                deshabilitar();
-                return totalPagar;    
-            case "medi":
-                totalPagar += 1000;
-                deshabilitar();
-                return totalPagar;  
-
-            case "gran":
-                totalPagar += 2000;
-                deshabilitar();
-                return totalPagar;  
-
-            case "":
-                swal("Error", "Debes seleccionar una tamaño de pizza", "error");
-                break
-        }
+    function revertir(numero) {
+        const convertir = numero.toString().split("").reverse().join("");
+        return Number(convertir);
     }
     let form = document.querySelector("#form");
     document.querySelector("#boton").disabled = false;
     let contenedor = document.querySelector("#contenedor");
-    let resultado = document.querySelector("#resultado");
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-        let checkbox = document.querySelectorAll("input[type='checkbox']");
-        let checkboxData = [];
-        checkbox.forEach(res =>(res.checked)? checkboxData.push(res.value):null);
-        let select = document.querySelector("#select").value;
-        let pagarTodo = pagar(checkboxData,select);
-        resultado.insertAdjacentHTML("beforebegin",`<h3>El total a pagar es de ${pagarTodo} pesos</h3>`);
+        let resultado = document.querySelector("#resultado");
+        let numero =Number(document.querySelector("#numero").value);
+        let convertido = revertir(numero);
+        resultado.insertAdjacentHTML("beforeend",convertido)
+        deshabilitar();
         form.reset();
     })
 })
