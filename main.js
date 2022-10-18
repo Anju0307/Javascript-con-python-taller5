6ea43b51166940263b2d398df0e2add7285eca3b
Anju0307 <jpmantilla01@misena.edu.co>
2022-10-18 17:47:48 -0500

    Ejercicio 7

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ addEventListener("DOMContentLoaded", (e) => {
     let form =document.querySelector("#form");
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-        let nombre = document.querySelector("#nombre").value;
-        let veces =parseInt(document.querySelector("#veces").value);
-        let conectados =parseInt(document.querySelector("#conectados").value);
-        let extra =parseInt(document.querySelector("#extra").value) ;
-        let sacrificios =parseInt(document.querySelector("#sacrificios").value) ;
-        let recibidas =parseInt(document.querySelector("#recibidas").value) ;
-        document.querySelector("#titRes").insertAdjacentHTML("beforeend", nombre)
-        document.querySelector("#resultado").insertAdjacentHTML("beforeend",((conectados+extra)/((veces-sacrificios-recibidas)*1000)));
+        let anterior = document.querySelector("#anterior").value;
+        let nuevo = document.querySelector("#nuevo").value;
+        anterior > nuevo ?
+            document.querySelector("#resultado").insertAdjacentHTML("beforeend","BAJO"):
+            anterior < nuevo ?
+                document.querySelector("#resultado").insertAdjacentHTML("beforeend","SUBIO"):
+                    document.querySelector("#resultado").insertAdjacentHTML("beforeend","ESTABLE") 
+
         habilitar();
         form.reset();
     })
